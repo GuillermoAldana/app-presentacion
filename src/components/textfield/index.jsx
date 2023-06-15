0aed324a715dd54c0e74e4051d03e7cde6b4b134
unknown <guillerm.aldana@gmail.com>
2023-06-15 20:02:36 -0300

    feat: create textfield component

---
+++ src/components/textfield/index.jsx
@@ -0,0 +1,24 @@
+import { TextField } from "@mui/material";
+import PropTypes from 'prop-types';
+
+const TextfieldPresentacion = (props) => {
+    const {name, value, onChange} = props;
+    return ( 
+        <TextField
+          id={name}
+          label={name}
+          value={value}
+          onChange={onChange}
+          name={name}
+          
+        />
+     );
+}
+ 
+export default TextfieldPresentacion;
+
+TextfieldPresentacion.propTypes = {
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func
+}
