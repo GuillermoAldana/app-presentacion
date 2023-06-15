3a963eae4cf94587fd86695dc64a2f3dcd6ad050
unknown <guillerm.aldana@gmail.com>
2023-06-15 19:57:24 -0300

    chore: implementation layout card to presentation

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,30 +1,31 @@
-
-import Card from '@mui/material/Card';
+import { useState } from 'react';
+import CardPresentacion from '../layout';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import ButtonPresentacion from '../button';
 import TitlePresentacion from '../typography';
 import ParagraphPresentacion from '../paragraph';
-import { useState } from 'react';
+
 import { IMAGE, OBJETO_INICIAL } from '../../constants/config';
+
 
 export default function Presentacion() {
   const [data, setData] = useState(OBJETO_INICIAL);
 
   const handleClick = () => {
     setData({
-      title: 'Practica de desarrollo', 
-      description:`SharePoint, es una herramienta software, accesible a través de navegador, que permite gestionar todo el contenido y documentación existente en cualquier empresa.`
-    });  
+      title: 'Practica de desarrollo',
+      description: `SharePoint, es una herramienta software, accesible a través de navegador, que permite gestionar todo el contenido y documentación existente en cualquier empresa.`
+    });
   }
 
   const handleCancelClick = () => {
-    setData(OBJETO_INICIAL);  
+    setData(OBJETO_INICIAL);
   }
-  
+
   return (
-    <Card sx={{ maxWidth: 545 }}>
+    <CardPresentacion>
       <CardMedia
         sx={{ height: 240 }}
         image={IMAGE}
@@ -35,9 +36,9 @@ export default function Presentacion() {
         <ParagraphPresentacion text={data.description} />
       </CardContent>
       <CardActions>
-        <ButtonPresentacion text='Enviar' tipo='small' onClick={handleClick}/>
-        <ButtonPresentacion text='Cancelar' tipo='small' onClick={handleCancelClick}/>
+        <ButtonPresentacion text='Enviar' tipo='small' onClick={handleClick} />
+        <ButtonPresentacion text='Cancelar' tipo='small' onClick={handleCancelClick} />
       </CardActions>
-    </Card>
+    </CardPresentacion>
   );
 }
