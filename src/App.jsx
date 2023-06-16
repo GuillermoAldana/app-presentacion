be4b7931a6e50097e4a546d386d707a47631244b
unknown <guillerm.aldana@gmail.com>
2023-06-15 23:30:09 -0300

    chore: adding list map to view userList

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { Grid, Typography } from "@mui/material"
-import FormularioPresentacion from "./components/forms"
-import { useState } from "react"
+import { CardContent, Grid, Typography } from "@mui/material";
+import { useState } from "react";
+import CardPresentacion from "./components/layout";
+import FormularioPresentacion from "./components/forms";
+import ChipAlert from "./components/chip";
 
 function App() {
-  const [ listOfUser, setListOfUser] = useState([]);
+  const [listOfUser, setListOfUser] = useState([]);
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={6} display="flex" justifyContent="center" alignItems="center">
-       {/*  <Presentacion /> */}
-       <FormularioPresentacion listOfUser={listOfUser} setListOfUser={setListOfUser}/>
+        {/*  <Presentacion /> */}
+        <FormularioPresentacion listOfUser={listOfUser} setListOfUser={setListOfUser} />
       </Grid>
-      <Grid item xs={12} sm={6} display="flex" justifyContent="center" alignItems="center">
-      {listOfUser.map((element, index) => {
-        return(
-          <div key={index}>
-            <Typography variant="h5" color="initial">{element.Nombre} {element.Apellido}</Typography>
-            <Typography variant="subtitle1" color="initial">{element.Nota}</Typography>
-          </div>
-        )
-      })}
+      <Grid item xs={12} sm={6}>
+        {listOfUser.map((element, index) => {
+          return (
+            <CardPresentacion key={index}>
+              <CardContent>
+                <Typography variant="h5" color="initial">{element.Nombre} {element.Apellido}</Typography>
+                <Typography variant="subtitle1" color="initial">{element.Nota}</Typography>
+                <ChipAlert estado={element.Estado}/>
+              </CardContent>
+            </CardPresentacion>
+          )
+        })}
       </Grid>
     </Grid>
   )
