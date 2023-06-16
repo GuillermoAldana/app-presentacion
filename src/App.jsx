9abd5dbe61fc9f47c08332d2527b4c73b04fb5ed
unknown <guillerm.aldana@gmail.com>
2023-06-15 23:11:54 -0300

    chore: implementation the grid to form and userlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { Box } from "@mui/material"
-import Presentacion from "./components/card"
+import { Grid, Typography } from "@mui/material"
+import FormularioPresentacion from "./components/forms"
+import { useState } from "react"
 
 function App() {
+  const [ listOfUser, setListOfUser] = useState([]);
   return (
-    <>
-      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-        <Presentacion />
-      </Box>
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6} display="flex" justifyContent="center" alignItems="center">
+       {/*  <Presentacion /> */}
+       <FormularioPresentacion listOfUser={listOfUser} setListOfUser={setListOfUser}/>
+      </Grid>
+      <Grid item xs={12} sm={6} display="flex" justifyContent="center" alignItems="center">
+      {listOfUser.map((element, index) => {
+        return(
+          <div key={index}>
+            <Typography variant="h5" color="initial">{element.Nombre} {element.Apellido}</Typography>
+            <Typography variant="subtitle1" color="initial">{element.Nota}</Typography>
+          </div>
+        )
+      })}
+      </Grid>
+    </Grid>
   )
 }
 
